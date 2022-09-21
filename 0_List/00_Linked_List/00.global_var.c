442ceada91946a37995a0be03ca4ea0349197c94
Abell-Choi <abellelus7074@gmail.com>
2022-09-22 08:45:51 +0900

    add getNodeLength function

--- 0_List/00_Linked_List/00.global_var.c
+++ 0_List/00_Linked_List/00.global_var.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//making struct
+// making struct
 typedef struct Node {
   int data;
-  struct Node *next;  // pointing next node
-}node;
-node *head;          // making head in globla
+  struct Node *next; // pointing next node
+} node;
+node *head; // making head in globla
 
-void createNode( int data );
+void createNode(int data);
 void printNode();
+void insertNode(int location, int data);
+int getNodeLength();
 
 int main() {
-  if (head == NULL) { printf("asdf"); }
-  // making first data
-  int i = 0;
-  while(i<10){
+  // if (head == NULL) { printf("asdf"); }
+  // making default data
+  for (int i = 0; i < 10; i++) {
     createNode(i);
-    i++;
   }
+
+  // printing node
   printNode();
+  int length = getNodeLength();
+  printf("length : %d", length);
+  // insertNode(1, 100);
 }
 
-void printNode(){
+// printing node function
+void printNode() {
   node *temp = head;
   printf("List : ");
-  while (temp != NULL){
+  while (temp != NULL) {
     printf("%d ", temp->data);
     temp = temp->next;
   }
   printf("\n");
 }
 
-void createNode( int data ){
+// creating node function
+void createNode(int data) {
   node *tmp = malloc(sizeof(node));
   tmp->data = data;
   tmp->next = head;
   head = tmp;
+}
+
+// 재사용성
+int getNodeLength() {
+  node *temp = head;
+  int i = -1;
+  while (temp != NULL) {
+    i++;
+    temp = temp->next;
+    printf("console i check -> %d\n", i);
+  }
+
+  return i + 1;
 }
